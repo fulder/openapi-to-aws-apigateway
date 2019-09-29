9ecfb9a5e602714ec1c79caf2c4fd413eed7cb60
fulder <misad90@gmail.com>
2019-09-29 10:59:06 +0200

    Add function to dump CloudFormation

--- generator.py
+++ generator.py
@@ -1,37 +1,42 @@
 import argparse
 import json
+import os
 
 import yaml
 
+CURRENT_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
 class Generator:
 
     def __init__(self, openapi_path):
         self.openapi_path = openapi_path
         self.docs = None
+        self.cloudformation_path = os.path.abspath(os.path.join(CURRENT_FOLDER, "apigateway.yaml"))
+        self.cloudformation = {"AWSTemplateFormatVersion": "2010-09-09", "Resources": {}}
 
     def generate(self):
         self._load_file()
 
+        self._save_cloudformation()
+
     def _load_file(self):
-        with open(self.openapi_path) as f:
+        with open(self.openapi_path, "r") as f:
             if ".json" in self.openapi_path:
                 self.docs = json.load(f)
             else:
                 self.docs = yaml.safe_load(f)
 
+    def _save_cloudformation(self):
+        with open(self.cloudformation_path, "w") as f:
+            yaml.safe_dump(self.cloudformation, f)
+
 
 def main():
-    args = parse_args()
-    generator = Generator(args.file)
-
-    generator.generate()
-
-
-def parse_args():
     parser = argparse.ArgumentParser(description="Generate AWS ApiGateway CloudFormation from OpenAPI specification")
     parser.add_argument("--file", "-f", required=True, type=str, help="Path to the OpenAPI specification file")
-    return parser.parse_args()
+    args = parser.parse_args()
+    generator = Generator(args.file)
+    generator.generate()
 
 
 if __name__ == "__main__":
