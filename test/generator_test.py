afe896952fb59f209901b0a74c7d0720cf31b4ef
fulder <misad90@gmail.com>
2019-10-09 22:02:25 +0200

    Fix failing tests

--- test/generator_test.py
+++ test/generator_test.py
@@ -2,7 +2,7 @@ import logging
 import os
 import unittest
 
-from generator.generator import Generator, CURRENT_FOLDER
+from generator.generator import Generator, CURRENT_FOLDER, VerbExtender
 
 logger = logging.getLogger("generator.generator")
 logger.addHandler(logging.StreamHandler())
@@ -12,122 +12,72 @@ logger.setLevel("DEBUG")
 class TestGenerator(unittest.TestCase):
 
     def test_determine_backend_type_http(self):
-        generator = Generator("test_swagger.json", "http://my-backend", False, "")
+        generator = Generator("test_swagger.json", "http://my-backend", False, "", "eu-west-1")
         generator._determine_backend_type()
         self.assertEqual("http", generator.backend_type)
 
     def test_determine_backend_type_http_proxy(self):
-        generator = Generator("test_swagger.json", "http://my-backend", True, "")
+        generator = Generator("test_swagger.json", "http://my-backend", True, "", "eu-west-1")
         generator._determine_backend_type()
         self.assertEqual("http_proxy", generator.backend_type)
 
     def test_determine_backend_type_aws(self):
-        generator = Generator("test_swagger.json", "arn:test:lambda:arn", False, "")
+        generator = Generator("test_swagger.json", "arn:test:lambda:arn", False, "", "eu-west-1")
         generator._determine_backend_type()
         self.assertEqual("aws", generator.backend_type)
 
     def test_determine_backend_type_aws_proxy(self):
-        generator = Generator("test_swagger.json", "arn:test:lambda:arn", True, "")
+        generator = Generator("test_swagger.json", "arn:test:lambda:arn", True, "", "eu-west-1")
         generator._determine_backend_type()
         self.assertEqual("aws_proxy", generator.backend_type)
 
     def test_init_integration_internet_type(self):
-        generator = Generator("test_swagger.json", "arn:test:lambda:arn", True, "")
-        generator._determine_backend_type()
-        integration = generator._init_integration()
-        exp_integration = {
-            "responses": {},
+        verb_extender = VerbExtender("get", {}, "/path1", "aws_proxy", "", True, "TEST_URI_START")
+        verb_extender._init_integration()
+        exp_verb = {
             "connectionType": "INTERNET",
+            "httpMethod": "POST",
             "type": "aws_proxy",
-            "requestParameters": {}
+            "uri": "TEST_URI_START"
         }
-        self.assertEqual(exp_integration, integration)
+        self.assertEqual(exp_verb, verb_extender.integration)
 
     def test_init_integration_vpc_type(self):
-        generator = Generator("test_swagger.json", "http://vpc_endpoint", True, "VPC_LINK_ID")
-        generator._determine_backend_type()
-        integration = generator._init_integration()
-        exp_integration = {
-            "responses": {},
+        verb_extender = VerbExtender("get", {}, "/path1", "http_proxy", "VPC_LINK_ID", True, "TEST_URI_START")
+        verb_extender._init_integration()
+        exp_verb = {
             "connectionId": "VPC_LINK_ID",
             "connectionType": "VPC_LINK",
+            "httpMethod": "POST",
             "type": "http_proxy",
-            "requestParameters": {},
+            "uri": "TEST_URI_START"
         }
-        self.assertEqual(exp_integration, integration)
+        self.assertEqual(exp_verb, verb_extender.integration)
 
-    def test_create_integration_creates_correct_verb(self):
-        generator = Generator("test_swagger.json", "http://my-backend", True, "")
-        generator._determine_backend_type()
-        integration = generator._init_integration()
-        verb = {}
-        generator._create_integration("TEST_VERB", "/path1", verb, integration)
+    def test_init_integration_creates_correct_verb(self):
+        verb_extender = VerbExtender("get", {}, "/path1", "http_proxy", "", False, "http://${stageVariables.httpHost}")
+        verb_extender._init_integration()
         exp_verb = {
-            "x-amazon-apigateway-integration": {
-                "connectionType": "INTERNET",
-                "httpMethod": "TEST_VERB",
-                "responses": {},
-                'requestParameters': {},
-                "type": "http_proxy",
-                "uri": "http://my-backend/path1"
-            }
+            "connectionType": "INTERNET",
+            "httpMethod": "GET",
+            "type": "http_proxy",
+            "uri": "http://${stageVariables.httpHost}/path1"
         }
-        self.assertEqual(exp_verb, verb)
+        self.assertEqual(exp_verb, verb_extender.integration)
 
-    def test_create_integration_with_lambda_creates_post_method(self):
-        generator = Generator("test_swagger.json", "arn:lambda", True, "")
-        generator._determine_backend_type()
-        integration = generator._init_integration()
-        verb = {}
-        generator._create_integration("TEST_VERB", "/path1", verb, integration)
+    def test_init_integration_with_lambda_creates_post_method(self):
+        verb_extender = VerbExtender("get", {}, "/path1", "aws", "", True, "TEST_START_URL")
+        verb_extender._init_integration()
         exp_verb = {
-            "x-amazon-apigateway-integration": {
-                "connectionType": "INTERNET",
-                "httpMethod": "POST",
-                "responses": {},
-                "requestParameters": {},
-                "type": "aws_proxy",
-                "uri": "arn:lambda"
-            }
+            "connectionType": "INTERNET",
+            "httpMethod": "POST",
+            "type": "aws",
+            "uri": "TEST_START_URL"
         }
-        self.assertEqual(exp_verb, verb)
-
-    def test_create_integration_with_responses(self):
-        generator = Generator("test_swagger.json", "http://my-backend", True, "")
-        generator._determine_backend_type()
-        integration = generator._init_integration()
-        verb = {
-            "responses": {
-                "200": {},
-                "400": {}
-            }
-        }
-        generator._create_integration("TEST_VERB", "/path1", verb, integration)
-        exp_verb = {
-            "responses": {
-                "200": {},
-                "400": {}
-            },
-            "x-amazon-apigateway-integration": {
-                "connectionType": "INTERNET",
-                "httpMethod": "TEST_VERB",
-                "responses": {
-                    "200": {
-                        "statusCode": "200"
-                    },
-                    "400": {
-                        "statusCode": "400"
-                    }
-                },
-                "requestParameters": {},
-                "type": "http_proxy",
-                "uri": "http://my-backend/path1"
-            }
-        }
-        self.assertEqual(exp_verb, verb)
+        self.assertEqual(exp_verb, verb_extender.integration)
 
     def test_docs_version_swagger(self):
-        generator = Generator("test_swagger.json", "http://my-backend", True, "")
+        generator = Generator("test_swagger.json", "http://my-backend", True, "", "eu-west-1")
         generator.docs = {
             "swagger": "2.0"
         }
@@ -136,7 +86,7 @@ class TestGenerator(unittest.TestCase):
         self.assertEqual(os.path.join(CURRENT_FOLDER, "out", "swagger.yaml"), generator.output_path_openapi)
 
     def test_docs_version_openapi(self):
-        generator = Generator("test_swagger.json", "http://my-backend", True, "")
+        generator = Generator("test_swagger.json", "http://my-backend", True, "", "eu-west-1")
         generator.docs = {
             "openapi": "3.0"
         }
@@ -145,7 +95,7 @@ class TestGenerator(unittest.TestCase):
         self.assertEqual(os.path.join(CURRENT_FOLDER, "out", "openapi.yaml"), generator.output_path_openapi)
 
     def test_docs_version_unsupported(self):
-        generator = Generator("test_swagger.json", "http://my-backend", True, "")
+        generator = Generator("test_swagger.json", "http://my-backend", True, "", "eu-west-1")
         generator.docs = {
             "invalid": "2.0"
         }
