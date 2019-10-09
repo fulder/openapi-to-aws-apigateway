3f2edd639ec63009cecba7ee2dee033ef1e22b8a
fulder <misad90@gmail.com>
2019-10-09 22:39:24 +0200

    Put main in own script

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 .PHONY: test-coverage
 test-coverage:
-	PYTHONPATH=. pytest --log-level DEBUG test/ --cov=generator.generator --cov=generator.verb_extender && coverage html
+	PYTHONPATH=. pytest --log-level DEBUG test/ --cov=generator && coverage html
--- generator/generator.py
+++ generator/generator.py
@@ -155,26 +155,3 @@ class Generator:
         with open(self.output_path_sam, "w") as f:
             yaml.safe_dump(self.cloudformation, f, default_flow_style=False, sort_keys=False)
         logger.info("Saved SAM template file to: [%s]", self.output_path_sam)
-
-
-def main():
-    parser = argparse.ArgumentParser(description="Generate AWS ApiGateway CloudFormation from OpenAPI specification")
-    # Required params
-    parser.add_argument("--file", "-f", required=True, type=str, help="Path to the OpenAPI specification file")
-    parser.add_argument("--backend_url", "-u", required=True, type=str,
-                        help="Backend URL to forward the requests to (use ARN for lambda backend)")
-
-    # Optional params
-    parser.add_argument("--apigateway_region", "-r", required=False,
-                        help="Region where ApiGateway will be deployed. Only needed for lambda integration")
-    parser.add_argument("--proxy", "-p", required=False, action="store_true", help="Proxy all requests to the backend")
-    parser.add_argument("--vpc_link_id", "-v", required=False, help="If backend is an VPC link, provide the link ID")
-    args = parser.parse_args()
-    generator = Generator(args.file, args.backend_url, args.proxy, args.vpc_link_id, args.apigateway_region)
-    generator.generate()
-
-
-if __name__ == "__main__":
-    logger.addHandler(logging.StreamHandler())
-    logger.setLevel("DEBUG")
-    main()
