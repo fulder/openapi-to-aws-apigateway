91ddf6ca0ba64369bc5a29440d1b880b82c323a3
fulder <misad90@gmail.com>
2019-10-20 20:50:38 +0200

    Add first version of setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oai-sam-api",
+    version="0.0.1",
+    author="Michal Sadowski",
+    author_email="misad90@gmail.com",
+    description="Generate SAM template and AWS OpenAPI extended docs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fulder/openapi-to-aws-apigateway",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.6, >=3.3',
+)
