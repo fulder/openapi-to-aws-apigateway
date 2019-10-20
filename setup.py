0af04f78a08319197852d42886b69dfa96f8bfc4
fulder <misad90@gmail.com>
2019-10-21 00:14:18 +0200

    Remove coverage travis stage

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="oai-sam",
-    version="0.0.3",
+    version="0.0.1",
     author="Michal Sadowski",
     entry_points={
         "console_scripts": [
